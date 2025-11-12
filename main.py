9fbb770df130d21856e60a73020c0ebbeed0d1b7
muhaimanamil-png <Muhaimanamil@gmail.com>
2025-11-12 12:54:35 +0800

    Feat: main

---
+++ main.py
@@ -0,0 +1,30 @@
+from utils import ensure_data_files
+from auth import login
+from menus import admin_menu, kasir_menu
+
+def main():
+    ensure_data_files()
+    print("=== Selamat Datang di WoWallet ===")
+    while True:
+        print("\nPilih jenis login:")
+        print("1. Login sebagai Admin")
+        print("2. Login sebagai Kasir")
+        print("0. Keluar")
+        opt = input("Pilih: ").strip()
+
+        if opt == "1":
+            user = login("Admin")
+            if user:
+                admin_menu(user)
+        elif opt == "2":
+            user = login("Kasir")
+            if user:
+                kasir_menu(user)
+        elif opt == "0":
+            print("Keluar dari aplikasi. Sampai jumpa!")
+            break
+        else:
+            print("Pilihan tidak valid.")
+
+if __name__ == "__main__":
+    main()
